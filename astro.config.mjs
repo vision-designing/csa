58e0dc83dc11b8f323a7c3198ee96898525c2267
kevinzunigacuellar <kevinzunigacuellar@gmail.com>
2025-02-12 11:26:58 -0500

    remove sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import awsAmplify from "astro-aws-amplify";
 
 export default defineConfig({
   site: "https://www.criticalsystemsanalysis.com",
-  output: "static",
   adapter: awsAmplify(),
   integrations: [sitemap()],
+  image: {
+    service: passthroughImageService(),
+  },
   vite: {
     plugins: [tailwindcss()],
   },
