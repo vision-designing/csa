cd00907a7ffbe3248b183e3750394f63b950b0fd
kevinzunigacuellar <kevinzunigacuellar@gmail.com>
2025-02-12 11:16:56 -0500

    upgrade to astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import awsAmplify from "astro-aws-amplify";
 
 export default defineConfig({
   site: "https://www.criticalsystemsanalysis.com",
-  output: "hybrid",
+  output: "static",
   adapter: awsAmplify(),
   integrations: [sitemap()],
-  image: {
-    service: squooshImageService(),
-  },
   vite: {
     plugins: [tailwindcss()],
   },
