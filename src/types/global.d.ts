9f295a85eff53997accce0a527cd2484e788b6fa
Sandeepa <chemilthas@gmail.com>
2025-10-09 09:33:57 +0530

    added google reCaptcha to contact form and careers form

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+// Global type declarations for reCAPTCHA
+declare global {
+  interface Window {
+    grecaptcha?: {
+      ready: (callback: () => void) => void;
+      execute: (siteKey: string, options: { action: string }) => Promise<string>;
+    };
+  }
+}
+
+export {};
