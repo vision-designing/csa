a079ea7d49f8649a60968e2837cf80ab4e9d2919
Krishan Apshaya <117499008+Apshaya@users.noreply.github.com>
2025-11-25 14:55:29 +0530

    update defaultMeta description and remove Consulting service from services list

--- src/content/config.ts
+++ src/content/config.ts
@@ -22,7 +22,7 @@ export const bookingLink =
 export const defaultMeta = {
   title: "Critical Systems Analysis | Functional Safety Consulting Experts",
   description:
-    "Expert functional safety engineers providing comprehensive safety compliance solutions, FMEA and FTA support consulting for safety critical systems and autonomous vehicles.",
+    "Expert functional safety engineers providing comprehensive safety compliance solutions, FMEA and FTA support for safety critical systems and autonomous vehicles.",
   og: {
     url: "/og.webp",
     alt: "A robot with some gears",
